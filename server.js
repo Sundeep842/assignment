46b39168d18863b4770d4237d098c1dc27cd55c4
sandeep <reddymallesundeep@gmail.com>
2020-12-31 12:47:14 +0000

    second hour with mocha

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ dotenv.config({path:'config.env'})
 app.use(morgan('tiny'))
 
 app.get('/',(req,res)=>{
-    res.send('crud application')
+     res.send('crud application')
 })
 
 app.listen(PORT,()=>{console.log("server is running")})
 
+module.exports=app
